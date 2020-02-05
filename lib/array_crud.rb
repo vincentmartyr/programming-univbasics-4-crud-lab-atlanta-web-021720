d780740a5516225ea09b25be45f80cfc81afc43c
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-05 21:09:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,7 +30,9 @@ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+array = ["hungry", "i", "am"]
+index_number = 2
+array[2]
 end
 
 def retrieve_first_element_from_array(array)
