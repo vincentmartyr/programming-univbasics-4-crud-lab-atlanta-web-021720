7c8d497aff227c1025f8eeee0f016a10ac13bac3
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-05 21:11:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -41,7 +41,8 @@ array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+array = ["I", "love", "you", "arrays!"]
+array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
